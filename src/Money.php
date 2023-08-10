019631adb255c832f45d30237f813e7438772d94
Benedikt Lenzen <36764562+DemigodCode@users.noreply.github.com>
2023-08-10 15:32:15 +0200

    Update Money.php

--- src/Money.php
+++ src/Money.php
@@ -105,7 +105,7 @@ class Money implements \JsonSerializable
      * @return mixed data which can be serialized by <b>json_encode</b>,
      * @link   http://php.net/manual/en/jsonserializable.jsonserialize.php
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return [
             'amount'   => $this->amount,
